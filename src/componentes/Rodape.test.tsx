31861407d54e0d1e01bf5a8a2475f783f0631deb
Ana-Dare <analauradare467@gmail.com>
2025-09-03 08:21:37 -0300

    test result draw

--- src/componentes/Rodape.test.tsx
+++ src/componentes/Rodape.test.tsx
@@ -10,10 +10,17 @@ jest.mock("../state/hook/useListaDeParticipantes", () => {
 });
 
 const mockNavegacao = jest.fn();
+const mockSorteio = jest.fn();
 
 jest.mock("react-router-dom", () => {
   return {
     useNavigate: () => mockNavegacao,
+  };
+});
+
+jest.mock("../state/hook/useSorteador", () => {
+  return {
+    useSorteador: () => mockSorteio,
   };
 });
 
@@ -61,5 +68,6 @@ describe("quantidade de participantes", () => {
 
     expect(mockNavegacao).toHaveBeenCalledTimes(1);
     expect(mockNavegacao).toHaveBeenCalledWith("/sorteio");
+    expect(mockSorteio).toHaveBeenCalledTimes(1);
   });
 });
--- src/paginas/Sorteio.test.tsx
+++ src/paginas/Sorteio.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
 import Sorteio from "./Sorteio";
+import { useResultadoSorteio } from "../state/hook/useResultadoSorteio";
 
 const mockNavegacao = jest.fn();
 
@@ -11,22 +12,59 @@ jest.mock("react-router-dom", () => {
   };
 });
 
+jest.mock("../state/hook/useResultadoSorteio", () => {
+  return {
+    useResultadoSorteio: jest.fn(),
+  };
+});
+
 jest.mock("../state/hook/useListaDeParticipantes");
 
 describe("pagian de sorteio", () => {
-  const participantes = ["ana", "clara", "julia", "tais"];
+  const participantes = ["ana", "clara", "julia"];
+
+  const resultado = new Map([
+    ["ana", "clara"],
+    ["clara", "julia"],
+    ["julia", "ana"],
+  ]);
+
   beforeEach(() => {
     (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+    (useResultadoSorteio as jest.Mock).mockReturnValue(resultado);
   });
 
   test("todos os participantes podem exibir seu amigo secreto", () => {
     render(
       <RecoilRoot>
-        <Sorteio/>
+        <Sorteio />
       </RecoilRoot>
     );
 
     const opcoes = screen.queryAllByRole("option");
     expect(opcoes).toHaveLength(participantes.length);
+  });
+
+  test("o amigo secreto é exibido", () => {
+    render(
+      <RecoilRoot>
+        <Sorteio />
+      </RecoilRoot>
+    );
+
+    const select = screen.getByPlaceholderText("Selecione o seu nome");
+
+    fireEvent.change(select, {
+      target: {
+        value: participantes[0],
+      },
+    });
+
+    const botao = screen.getByRole("button");
+    fireEvent.click(botao);
+
+    const amigoSecreto = screen.getByRole("alert");
+
+    expect(amigoSecreto).toBeInTheDocument();
   });
 });
